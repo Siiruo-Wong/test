ac4c165f83e6b8551a5294badec5a94ff3189889
Siiruo-Wong <yanjwang@foxmail.com>
2019-03-21 23:34:44 +0800

    test rebase

---
+++ src/main/java/com/siiruo/bytecode/modification/TestRebase.java
@@ -0,0 +1,10 @@
+package com.siiruo.bytecode.modification;
+
+/**
+ * Created by siiruo wong on 2019/3/21.
+ */
+public class TestRebase {
+    public static void main(String[] args) {
+        System.out.println("test rebase 001");
+    }
+}
